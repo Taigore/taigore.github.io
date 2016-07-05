4c352f0dda9d361da9a2df36ead4e4349f5f6c48
Federico Bellia <f.bellia@hotmail.it>
2016-07-05 16:03:06 +0200

    Builds main layout of site

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
 if (jQuery == null) throw "JQuery not loaded";
-
-$(document).ready(function() {
-    $('body')
-        .append('<table id="game-field">@</table'
-            .replace('@', '<tr>@</tr>'
-                .replace('@', '<td></td>'
-                    .repeat(7))
-                .repeat(7)));
-});
